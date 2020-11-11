c86c7d1f5285efb2744492a0415a0737bb340151
gowdzy <74279474+gowdzy@users.noreply.github.com>
2020-11-11 12:57:14 +0800

    enter

--- assets/scripts/login/LoginProxy.ts
+++ assets/scripts/login/LoginProxy.ts
@@ -17,13 +17,16 @@ export class LoginProxy {
     }
 
     public  init(){
-        cc.systemEvent.on(ServerConfig.account_login, this.login, this);
         cc.systemEvent.on(HttpConfig.register.name, this.register, this);
+        cc.systemEvent.on(ServerConfig.account_login, this.login, this);
         cc.systemEvent.on(ServerConfig.role_enterServer, this.enter, this);
 
 
         cc.systemEvent.on(NetEvent.ServerConnected, this.conneted, this);
         cc.systemEvent.on(ServerConfig.account_reLogin, this.relogin, this);
+
+
+        cc.systemEvent.on(ServerConfig.role_create, this.create, this);
     }
 
 
@@ -46,13 +49,18 @@ export class LoginProxy {
 
     private enter(data){
         console.log("LoginProxy  enter:",data);
+
+        //没有创建打开创建
         if(data.code == 9){
             cc.systemEvent.emit("create");
+        }else{
+            //进入游戏
         }
     }
 
 
     private relogin(data){
+        //断线重新登录
         console.log("LoginProxy  relogin:",data);
     }
 
@@ -62,6 +70,7 @@ export class LoginProxy {
 
 
     private conneted(){
+        //连接成功
         var loginData = this.getLoginData();
         console.log("LoginProxy  conneted:",loginData);
         if(loginData){
@@ -69,6 +78,13 @@ export class LoginProxy {
         }
     }
 
+
+    private create(data){
+        //重换成功再次调用
+        if(data.code == 0){
+            new LoginCommand().role_enterServer(0);
+        }
+    }
 
     public clear(){
         this._loginData = null;
