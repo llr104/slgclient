669ee20fe729dc6657609e7452187a73db8a32ff
pengxch <624108274@qq.com>
2020-11-20 16:03:07 +0800

    serverCfg整理

--- assets/scripts/config/ServerConfig.ts
+++ assets/scripts/config/ServerConfig.ts
@@ -1,29 +1,29 @@
 /**服务器接口配置*/
 const ServerConfig = {
     account_login: "account.login",
+    account_logout: "account.logout",
+    account_reLogin: "account.reLogin",
+    
     role_create: "role.create",
     role_roleList: "role.roleList",
     role_enterServer: "role.enterServer",
-    account_logout:"account.logout",
-    account_reLogin: "account.reLogin",
-
     role_myCity: "role.myCity",
-    role_myRoleRes:"role.myRoleRes",
+    role_myRoleRes: "role.myRoleRes",
+    role_roleBuildState: "role.roleBuildState",
+    role_myProperty: "role.myProperty",
+    
     nationMap_config: "nationMap.config",
     nationMap_scanBlock: "nationMap.scanBlock",
-    role_myProperty:"role.myProperty",
 
+    city_facilities: "city.facilities",
+    city_upFacility: "city.upFacility",
 
-    city_facilities:"city.facilities",
-    city_upFacility:"city.upFacility",
-
-
-    general_myGenerals:"general.myGenerals",
-    general_dispose:"general.dispose",
-    general_armyList:"general.armyList",
-    general_conscript:"general.conscript",
-    general_assignArmy:"general.assignArmy",
-    general_armyState:"general.armyState",
+    general_myGenerals: "general.myGenerals",
+    general_dispose: "general.dispose",
+    general_armyList: "general.armyList",
+    general_conscript: "general.conscript",
+    general_assignArmy: "general.assignArmy",
+    general_armyState: "general.armyState",
 
 }
 
