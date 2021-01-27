e1893434b50148a738c688ea35e77fe4121236a2
llr104 <lianglirong1991@gmail.com>
2021-01-27 12:23:46 +0800

    地图上画了系统城池

---
+++ assets/resources/world/city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="city" tilewidth="200" tileheight="100" tilecount="16" columns="4">
+ <image source="town_big.png" width="812" height="406"/>
+</tileset>
---
+++ assets/resources/world/cityComponent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="cityComponent" tilewidth="200" tileheight="100" tilecount="190" columns="10">
+ <image source="cityComponent.png" width="2022" height="1928"/>
+</tileset>
